ed1563baf7c3d61c56086f5931b0745f30304600
garbinlin <garbinlin@gmail.com>
2015-07-24 00:02:54 +0800

    v0.2

--- gabin.js
+++ gabin.js
@@ -1,8 +1,33 @@
-(function() {
+(function(w) {
+  var document = w.document;
   // Construct
-  function Gabin() {
-	  
+  function Gabin(selector) {
+	  if (!selector) {
+		return [];
+	  }
+	  console.log(selector);
+	  // split space
+	  var selectors = selector.split(/\s/);
+	  var target = null;
+	  for (var i = 0; i < selector.length; i++) {
+		  	
+	  }
+	  function select(select) {
+		select = select || "";
+		select = Gabin.trim(select);
+		if (select.indexOf("#") === 0) {
+			return document.getElementById(select);
+		} else if (select.indexOf(".") === 0) {
+			return document.getElementsByClassName(select);
+		}
+	  }
   }
+  Gabin.trim = function(str) {
+	if (typeof str === "string" && str.trim) {
+		return str.trim();
+	}
+	return (str || "").toString().replace(/^\s+|\s$/g, "");
+  };
   var gabin = {
   };
   gabin.extend = function(a, b) {
@@ -10,5 +35,6 @@
 		  a[key] = b[key];
 	  }
 	  return a;
-  }
-} () );
+  };
+  w.$ = w.Gabin = Gabin;
+} (window) );
