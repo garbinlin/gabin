212469e7833297580f6f3b3ce534021e2f3337fa
garbinlin <garbinlin@gmail.com>
2015-07-26 10:57:07 +0800

    string parse

--- gabin.js
+++ gabin.js
@@ -5,7 +5,6 @@
 	  if (!selector) {
 		return [];
 	  }
-	  console.log(selector);
 	  // split space
 	  var selectors = selector.split(/\s/);
 	  var target = null;
@@ -28,6 +27,40 @@
 	}
 	return (str || "").toString().replace(/^\s+|\s$/g, "");
   };
+  Gabin.parseCookie = function(str) {
+    str = str || "";
+	var json = {
+	};
+	var list = str.split("; ");
+	for (var i = 0; i < list.length; i++) {
+		var one = list[i];
+		var p = one.indexOf("=");
+		var name = one.substring(0, p);
+		var value = one.substring(p + 1);
+		value = decodeURIComponent(value);
+		json[name] = value;
+	}
+	return json;
+  };
+  Gabin.parseQueryString = function(str) {
+    str = str || location.search;
+	str = str.replace("?", "&");
+	var json = {
+	};
+	var list = str.split("&");
+	for (var i = 0; i < list.length; i++) {
+		var one = list[i] || "";
+		var p = one.indexOf("=");
+		var name = one.substring(0, p);
+		var value = one.substring(p + 1);
+		json[name] = value;
+	}
+	return json;
+  };
+  Gabin.getUrlParam = function(key) {
+    var json = this.parseQueryString();
+	return json[key] || "";
+  };
   var gabin = {
   };
   gabin.extend = function(a, b) {
@@ -37,4 +70,4 @@
 	  return a;
   };
   w.$ = w.Gabin = Gabin;
-} (window) );
+} (window));
